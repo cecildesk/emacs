d37b8ba0b928a68b5f73bd5756c3beb7a3ec25b4
Cecil-PC\Cecil <deathk88@yahoo.com>
2015-06-24 20:27:17 +0900

    inital .emacs upload
    초기 설정 파일 업로드

---
+++ emacs.el
@@ -0,0 +1,41 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+
+(set-face-font 'default "Monaco-9")
+(set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+                  '("맑은 고딕" . "iso10646-1"))
+(set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
+                  '("맑은 고딕" . "iso10646-1"))
+
+(setq face-font-rescale-alist
+      '((".*nanum.*" . 1.3)
+        (".*맑은.*" . 1.0)))
+(set-default 'truncate-lines t)
+(global-linum-mode 1)
