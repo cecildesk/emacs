443fdbb378b13cfdc78acfc78f4a69e7c6408bc0
Cecil <cecildesk@gmail.com>
2015-07-10 17:39:54 +0900

    한글설정 및 web-mode , emmet-mode 설정 추가

--- emacs.el
+++ emacs.el
@@ -11,12 +11,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  )
 
+;; 한글폰트 관련 세팅
+(set-face-font 'default "Monaco-9")
+(set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+                  '("맑은 고딕" . "iso10646-1"))
+(set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
+                  '("맑은 고딕" . "iso10646-1"))
+
+(setq face-font-rescale-alist
+      '((".*nanum.*" . 1.3)
+        (".*맑은.*" . 1.0)))
+
+;;MELPA Package Install
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+;;Web-mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -27,15 +43,21 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+;;emmet mode hook 관련 세팅
+(require 'emmet-mode)
+(add-to-list 'load-path "~/Emacs/emmet/")
+;(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+;(add-hook 'html-mode-hook 'emmet-mode)
+;(add-hook 'css-mode-hook  'emmet-mode)
 
-(set-face-font 'default "Monaco-9")
-(set-fontset-font "fontset-default" '(#x1100 . #xffdc)
-                  '("맑은 고딕" . "iso10646-1"))
-(set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
-                  '("맑은 고딕" . "iso10646-1"))
 
-(setq face-font-rescale-alist
-      '((".*nanum.*" . 1.3)
-        (".*맑은.*" . 1.0)))
-(set-default 'truncate-lines t)
-(global-linum-mode 1)
+(setq emmet-preview-default t)
+(if (boundp 'web-mode-hook)
+    (add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'html-mode-hook 'emmet-mode))
+(add-hook 'css-mode 'emmet-mode)
+
+
+;;
+(set-default 'truncate-lines t)  ;; Word-Wrap 없애기 코팅할때 편하게
+(global-linum-mode 1)   ;; 라인넘버 무조건 표시하기 (코팅용이라 우선 이렇게)
